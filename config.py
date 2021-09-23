a62da91f57a19aa7ae254a78f1311055b7120590
Braxton Van Cleave <48543007+rbvancleave@users.noreply.github.com>
2021-09-22 19:45:09 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 leagueID = input('leagueID: ')
 swid = input("swid, include '{ }', ex. {DCIAD841-5201-444F-AAD8-414201A44F9A}: ")
-espn_s2 = input("espn_s2 (quite long..): "
+espn_s2 = input("espn_s2 (quite long..): ")
 
 
 
