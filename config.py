bd7970b6ffcfa4d79aa9efdd5e4519052f1bbbbb
Braxton Van Cleave <48543007+rbvancleave@users.noreply.github.com>
2021-09-23 10:00:00 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,11 @@ leagueID = getpass('leagueID: ')
 swid = getpass("swid, include '{ }', ex. {DCIAD841-5201-444F-AAD8-414201A44F9A}: ")
 espn_s2 = getpass("espn_s2 (quite long..): ")
 
+'''
+Hardcode variables and switch commenting
 
+# leagueID = ""
+# swid = ""
+# espn_s2 = "" 
+
+'''
