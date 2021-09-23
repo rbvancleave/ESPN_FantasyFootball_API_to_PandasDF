d7ac2a3b5bba6c4f5c167049127d00b0498abfb5
rbvancleave <48543007+rbvancleave@users.noreply.github.com>
2021-09-22 20:25:49 -0500

    added getpass() to leagueID, swid, and espn_s2

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-leagueID = input('leagueID: ')
-swid = input("swid, include '{ }', ex. {DCIAD841-5201-444F-AAD8-414201A44F9A}: ")
-espn_s2 = input("espn_s2 (quite long..): ")
+from getpass import getpass
+
+leagueID = getpass('leagueID: ')
+swid = getpass("swid, include '{ }', ex. {DCIAD841-5201-444F-AAD8-414201A44F9A}: ")
+espn_s2 = getpass("espn_s2 (quite long..): ")
 
 
